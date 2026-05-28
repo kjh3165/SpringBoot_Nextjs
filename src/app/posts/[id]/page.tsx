c1cc6bc736fdaf61e2ccfad487df37756c88c4d3
kjh3165 <jhwk109@gmail.com>
2026-05-28 12:47:19 +0900

    상세페이지 구현, 일단은 무조건 URL에 상관없이 1번글이 노출

---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
+export default function Page() {
+    const [post, setPost] = useState({})
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/v1/posts/1')
+            .then((res) => res.json())
+            .then((data) => setPost(data))
+    }, [])
+
+    return (
+        <>
+            <h1>게시글 상세페이지</h1>
+            <div>게시글 번호: {post.id}</div>
+            <div>게시글 제목: {post.title}</div>
+            <div>게시글 내용: {post.content}</div>
+        </>
+    )
+}
