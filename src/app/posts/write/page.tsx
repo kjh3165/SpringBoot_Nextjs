c8d5b3b679ddde0012080867d3408186329c7c09
kjh3165 <jhwk109@gmail.com>
2026-05-28 14:44:36 +0900

    글쓰기 유효성 체크

--- src/app/posts/write/page.tsx
+++ src/app/posts/write/page.tsx
@@ -1,8 +1,36 @@
+'use client'
+
 export default function Page() {
+    const handleSumbit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        const form = e.target as HTMLFormElement
+
+        const titleInput = form.elements.namedItem('title') as HTMLInputElement
+        const contentInput = form.elements.namedItem(
+            'content',
+        ) as HTMLTextAreaElement
+
+        if (titleInput.value.trim() === '' || titleInput.value.length === 0) {
+            alert('제목을 입력해주세요.')
+            titleInput.focus()
+            return
+        }
+
+        if (
+            contentInput.value.trim() === '' ||
+            contentInput.value.length === 0
+        ) {
+            alert('내용을 입력해주세요.')
+            contentInput.focus()
+            return
+        }
+    }
+
     return (
         <>
             <h1>글쓰기</h1>
-            <form className="flex flex-col gap-2 p-2">
+            <form className="flex flex-col gap-2 p-2" onSubmit={handleSumbit}>
                 <input
                     className="border p-2 rounded"
                     type="text"
