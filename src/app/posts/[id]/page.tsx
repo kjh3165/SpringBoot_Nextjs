06de0cce82df3f30bd047f8c113d172f8866cb88
kjh3165 <jhwk109@gmail.com>
2026-05-28 12:57:04 +0900

    {조건 && <컴포넌트 />} 로딩 효과 추가

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -20,9 +20,12 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
     return (
         <>
             <h1>게시글 상세페이지</h1>
-            <div>게시글 번호: {post?.id}</div>
-            <div>게시글 제목: {post?.title}</div>
-            <div>게시글 내용: {post?.content}</div>
+            {post === null && <div>로딩중...</div>}
+            <>
+                <div>게시글 번호: {post?.id}</div>
+                <div>게시글 제목: {post?.title}</div>
+                <div>게시글 내용: {post?.content}</div>
+            </>
         </>
     )
 }
