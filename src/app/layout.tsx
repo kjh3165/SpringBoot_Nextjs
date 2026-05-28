d328eebfbc1870c0714144b4ffe60392c97dba09
kjh3165 <jhwk109@gmail.com>
2026-05-28 12:30:14 +0900

    .prettierrc 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,34 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+    variable: '--font-geist-sans',
+    subsets: ['latin'],
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+    variable: '--font-geist-mono',
+    subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
-  title: "사이트 A",
-  description: "SpringBoot, Nextjs 연동",
-};
+    title: '사이트 A',
+    description: 'SpringBoot, Nextjs 연동',
+}
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="ko">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="ko">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    )
 }
