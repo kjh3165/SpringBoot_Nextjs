18a784975033d55efc636a21d63831175f7bb875
kjh3165 <jhwk109@gmail.com>
2026-05-29 09:25:12 +0900

    apiFetch 함수를 확장하여 POST 요청도 가능하도록

--- src/app/posts/write/page.tsx
+++ src/app/posts/write/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { apiFetch } from '@/lib/backend/client'
+
 export default function Page() {
     const handleSumbit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
@@ -26,7 +28,7 @@ export default function Page() {
             return
         }
 
-        fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/posts`, {
+        apiFetch(`/api/v1/posts`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -35,11 +37,7 @@ export default function Page() {
                 title: titleInput.value,
                 content: contentInput.value,
             }),
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                alert(data.msg)
-            })
+        }).then((res) => alert(res.msg))
     }
 
     return (
--- src/lib/backend/client.ts
+++ src/lib/backend/client.ts
@@ -1,5 +1,5 @@
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
 
-export const apiFetch = (url: string) => {
-    return fetch(`${API_BASE_URL}${url}`).then((res) => res.json())
+export const apiFetch = (url: string, options?: RequestInit) => {
+    return fetch(`${API_BASE_URL}${url}`, options).then((res) => res.json())
 }
