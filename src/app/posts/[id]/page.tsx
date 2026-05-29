d05404700f31e53342d6d6d383bb561c2a27f3ad
kjh3165 <jhwk109@gmail.com>
2026-05-29 14:28:13 +0900

    댓글 등록

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -43,6 +43,39 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
         )
     }
 
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        const form = e.target as HTMLFormElement
+
+        const contentInput = form.elements.namedItem(
+            'content',
+        ) as HTMLTextAreaElement
+
+        if (
+            contentInput.value.trim() === '' ||
+            contentInput.value.length === 0
+        ) {
+            alert('댓글 내용을 입력해주세요.')
+            contentInput.focus()
+            return
+        }
+
+        apiFetch(`/api/v1/posts/${id}/comments`, {
+            method: 'POST',
+            body: JSON.stringify({
+                content: contentInput.value,
+            }),
+        }).then((data) => {
+            alert(data.msg)
+            contentInput.value = ''
+
+            if (postComments == null) return
+
+            setPostComments([...postComments, data.data])
+        })
+    }
+
     useEffect(() => {
         apiFetch(`/api/v1/posts/${id}`).then(setPost)
         apiFetch(`/api/v1/posts/${id}/comments`).then(setPostComments)
@@ -74,6 +107,18 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
                 </Link>
             </div>
 
+            <h2>댓글 작성</h2>
+            <form className="flex flex-col gap-2 p-2" onSubmit={handleSubmit}>
+                <textarea
+                    className="border p-2 rounded"
+                    name="content"
+                    placeholder="댓글 내용"
+                />
+                <button className="border p-2 rounded" type="submit">
+                    작성
+                </button>
+            </form>
+
             <h2>댓글 목록</h2>
             {postComments === null && <div>댓글이 로딩중...</div>}
 
@@ -81,7 +126,7 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
                 <ul>
                     {postComments.map((comment) => (
                         <li key={comment.id}>
-                            {comment.content}
+                            {comment.id}/{comment.content}
                             <button
                                 className="p-2 rounded border"
                                 onClick={() =>
