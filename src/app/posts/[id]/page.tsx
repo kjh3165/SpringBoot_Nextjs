762786901b251e81ee5434d7223d2ed10956df9c
kjh3165 <jhwk109@gmail.com>
2026-05-29 10:21:00 +0900

    글 삭제 후, 메세지 출력 후 글 목록으로 이동, confirm 으로 삭제 여부 확인

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -2,12 +2,26 @@
 
 import { apiFetch } from '@/lib/backend/client'
 import { PostDto } from '@/types/post'
+import { useRouter } from 'next/navigation'
 import { use, useEffect, useState } from 'react'
 
 export default function Page({ params }: { params: Promise<{ id: number }> }) {
     const [post, setPost] = useState<PostDto | null>(null)
 
     const { id } = use(params)
+
+    const router = useRouter()
+
+    const deletePost = (id: number) => {
+        if (!confirm('정말 삭제하시겠습니까?')) return
+
+        apiFetch(`/api/v1/posts/${id}`, {
+            method: 'DELETE',
+        }).then((data) => {
+            alert(data.msg)
+            router.replace('/posts')
+        })
+    }
 
     useEffect(() => {
         apiFetch(`/api/v1/posts/${id}`).then(setPost)
@@ -23,6 +37,15 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
                 <div>게시글 제목: {post?.title}</div>
                 <div>게시글 내용: {post?.content}</div>
             </>
+
+            <div className="flex gap-2">
+                <button
+                    onClick={() => deletePost(post.id)}
+                    className="p-2 rounded border"
+                >
+                    삭제
+                </button>
+            </div>
         </>
     )
 }
