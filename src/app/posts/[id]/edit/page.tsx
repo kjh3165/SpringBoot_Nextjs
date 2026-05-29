aa0e827e059b35650cf3f17e11a24c775c905975
kjh3165 <jhwk109@gmail.com>
2026-05-29 11:24:02 +0900

    글 수정 폼에서 생성된 데이터로 수정 API에 호출

--- src/app/posts/[id]/edit/page.tsx
+++ src/app/posts/[id]/edit/page.tsx
@@ -2,11 +2,51 @@
 
 import { apiFetch } from '@/lib/backend/client'
 import { PostDto } from '@/types/post'
+import { useRouter } from 'next/navigation'
 import { use, useEffect, useState } from 'react'
 
 export default function Page({ params }: { params: Promise<{ id: number }> }) {
     const { id } = use(params)
     const [post, setPost] = useState<PostDto | null>(null)
+
+    const router = useRouter()
+
+    const handleSumbit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        const form = e.target as HTMLFormElement
+
+        const titleInput = form.elements.namedItem('title') as HTMLInputElement
+        const contentInput = form.elements.namedItem(
+            'content',
+        ) as HTMLTextAreaElement
+
+        if (titleInput.value.trim() === '' || titleInput.value.length === 0) {
+            alert('제목을 입력해주세요.')
+            titleInput.focus()
+            return
+        }
+
+        if (
+            contentInput.value.trim() === '' ||
+            contentInput.value.length === 0
+        ) {
+            alert('내용을 입력해주세요.')
+            contentInput.focus()
+            return
+        }
+
+        apiFetch(`/api/v1/posts/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                title: titleInput.value,
+                content: contentInput.value,
+            }),
+        }).then((data) => {
+            alert(data.msg)
+            router.replace(`/posts/${id}`)
+        })
+    }
 
     useEffect(() => {
         apiFetch(`/api/v1/posts/${id}`).then(setPost)
@@ -18,7 +58,7 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
         <>
             <h1>{id}번 글 수정 페이지</h1>
 
-            <form className="flex flex-col gap-2 p-2">
+            <form className="flex flex-col gap-2 p-2" onSubmit={handleSumbit}>
                 <input
                     className="border p-2 rounded"
                     type="text"
